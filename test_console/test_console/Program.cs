10b9c30e7e54ebc7d10e1ec037158b646fabca77
JY0102 <58729002+JY0102@users.noreply.github.com>
2025-05-19 11:16:13 +0900

    Add files via upload

---
+++ test_console/test_console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            
+        }
+
+        private static void HelloWorld()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
